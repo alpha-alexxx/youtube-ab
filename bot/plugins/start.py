2417b4edca324910c34ee997bac3b949b95e4f6f
alpha-alexxx <65218056+alpha-alexxx@users.noreply.github.com>
2021-09-30 12:33:30 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -19,25 +19,6 @@ async def _start(c: UtubeBot, m: Message):
         text=tr.START_MSG.format(m.from_user.first_name),
         quote=True,
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Thank You", url="https://www.youtube.com/channel/UCHwnjZgBoKFSaxUhrLx1lWQ")]]
-        ),
-@UtubeBot.on_message(
-    Filters.private
-    & Filters.incoming
-    & Filters.command("url")
-    & Filters.user(Config.AUTH_USERS)
-)
-async def _url(c: UtubeBot, m: Message):
-    await m.reply_chat_action("typing")
-
-    await m.reply_text(
-        text=tr.START_MSG.format(m.from_user.first_name),
-        quote=True,
-        auth = GoogleAuth(Config.CLIENT_ID, Config.CLIENT_SECRET)
-        url = auth.GetAuthUrl()
-
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Direct Auth url", url=url)]]
+            [[InlineKeyboardButton("Join Project Channel!", url="https://t.me/odbots")]]
         ),
     )
-    
