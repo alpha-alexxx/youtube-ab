a40f4d64bdc791615a34055fe4d24aa530150e98
alpha-alexxx <65218056+alpha-alexxx@users.noreply.github.com>
2021-09-30 12:43:31 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -19,6 +19,6 @@ async def _start(c: UtubeBot, m: Message):
         text=tr.START_MSG.format(m.from_user.first_name),
         quote=True,
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Join Project Channel!", url="https://t.me/odbots")]]
+            [[InlineKeyboardButton("Direct Authorise Link", url="https://accounts.google.com/o/oauth2/v2/auth?client_id=1003241289696-f8dmkhhv7e1p5ogs6of4s3qrfr6g92j6.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fyoutube.upload&access_type=offline&response_type=code")]]
         ),
     )
